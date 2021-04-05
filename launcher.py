d519b180035106bc49cbd6b9a23ee6ae75b5743a
Vladimir <alfa.voland@gmail.com>
2021-04-05 17:25:15 +0300

    different commands

--- launcher.py
+++ launcher.py
@@ -3,16 +3,17 @@ import subprocess
 process = []
 
 while True:
-    action = input('Выберите действие: q - выход , s - запустить сервер и клиенты, x - закрыть все окна:')
+    action = input(
+        'Выберите действие: (q, й) - выход , (s, ы, д, l) - запустить сервер и клиенты, (x, ч) - закрыть все окна:')
 
-    if action == 'q':
+    if action in ['q', 'й']:
         break
-    elif action == 's':
+    elif action in ['s', 'ы', 'l', 'д']:
         process.append(subprocess.Popen('python server.py', creationflags=subprocess.CREATE_NEW_CONSOLE))
         process.append(subprocess.Popen('python client.py -n test1', creationflags=subprocess.CREATE_NEW_CONSOLE))
         process.append(subprocess.Popen('python client.py -n test2', creationflags=subprocess.CREATE_NEW_CONSOLE))
         process.append(subprocess.Popen('python client.py -n test3', creationflags=subprocess.CREATE_NEW_CONSOLE))
-    elif action == 'x':
+    elif action in ['x', 'ч']:
         while process:
             victim = process.pop()
             victim.kill()
